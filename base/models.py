9592cdba6f5b97a565155bfecda5500a7a0c1ab9
Diecaal <diecaal@gmail.com>
2022-03-06 21:40:15 +0100

    Restrict pages

--- base/models.py
+++ base/models.py
@@ -3,27 +3,32 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Topic(models.Model):
     '''
     A topic which will be assigned to one or more rooms
     '''
-    name = models.CharField(max_length = 200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Room(models.Model):
     '''
     Room class that will hold messages about a specific topic
     '''
-    #id = we can add the usage as UID for example
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null = True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null = True) # if we use SET_NULL approach, specificy that database can contain null for that value
-    name = models.CharField(max_length = 200)
-    description = models.TextField(null = True, blank = True) # allow empty field
-    #participants =
-    updated = models.DateTimeField(auto_now = True) # takes timestamp when is called/update [>= ONCE]
-    created = models.DateTimeField(auto_now_add = True) # takes a timestamp when method is created [ONCE]
+    # id = we can add the usage as UID for example
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # if we use SET_NULL approach, specificy that database can contain null for that value
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)  # allow empty field
+    # participants =
+    # takes timestamp when is called/update [>= ONCE]
+    updated = models.DateTimeField(auto_now=True)
+    # takes a timestamp when method is created [ONCE]
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-updated', '-created']
@@ -32,15 +37,17 @@ class Room(models.Model):
     def __str__(self):
         return str(self.name)
 
+
 class Message(models.Model):
     '''
     Message created by a user assigned to a room
     '''
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    room = models.ForeignKey(Room, on_delete = models.CASCADE) # When parent (room) all children (messages) will be deleted
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # When parent (room) all children (messages) will be deleted
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
-    updated = models.DateTimeField(auto_now = True) 
-    created = models.DateTimeField(auto_now_add = True) 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.body[0:50]) # First 50 characters of the messsage
+        return str(self.body[0:50])  # First 50 characters of the messsage
--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
 from django.db.models import Q
 from django.template import context
+from django.contrib.auth.decorators import login_required
 from .models import Room, Topic
 from .forms import RoomForm
 from django.contrib.auth.models import User
@@ -9,8 +11,13 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create views to be returned here
 
+
 def loginPage(request):
-    if( request.method == 'POST' ):
+    # No matter user writes login URL, if he is authenticated will go to home page
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if(request.method == 'POST'):
         username = request.POST.get('username')
         password = request.POST.get('password')
         try:
@@ -24,65 +31,81 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username or Password does not exist')
-    context = { }
+    context = {}
     return render(request, 'base/login_register.html', context)
+
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
 
+
 def home(request):
     # Default behaviour will return all topics
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     # Search room by their TOPIC name, NAME and DESCRIPTION
-    rooms = Room.objects.filter( 
-        Q(topic__name__icontains = q) |
-        Q(name__icontains = q) |
-        Q(description__icontains = q)
-    ) 
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
-    room_count_info = f'{rooms.count()} room(s) for: {q}' if q != '' else f'{rooms.count()} room(s) open to you ^^' 
+    room_count_info = f'{rooms.count()} room(s) for: {q}' if q != '' else f'{rooms.count()} room(s) open to you ^^'
     # Passing a dict 'rooms' to the view with our rooms list assigned
-    context = { 'rooms': rooms, 'topics': topics, 'room_count_info' : room_count_info } 
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count_info': room_count_info}
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
-    room = Room.objects.get(id = pk)
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+
+@login_required(login_url='login')
 def create_room(request):
     form = RoomForm()
 
-    if( request.method == 'POST' ):
+    if(request.method == 'POST'):
         form = RoomForm(request.POST)
-        if( form.is_valid() ):
+        if(form.is_valid()):
             form.save()
             return redirect('home')
 
-    context = { 'form': form }
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
-def update_room(request, pk):
-    room = Room.objects.get( id = pk )
-    form = RoomForm( instance = room )
 
-    if( request.method == 'POST' ):
-        form = RoomForm( request.POST, instance = room )
-        if( form.is_valid() ):
+@login_required(login_url='login')
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed to do this actions')
+
+    if(request.method == 'POST'):
+        form = RoomForm(request.POST, instance=room)
+        if(form.is_valid()):
             form.save()
             return redirect('home')
 
-    context = { 'form': form }
-    return render(request, 'base/room_form.html', context) 
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
 
+
+@login_required(login_url='login')
 def delete_room(request, pk):
-    room = Room.objects.get( id = pk )
-    context = { 'obj' : room }
+    room = Room.objects.get(id=pk)
+    context = {'obj': room}
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed to do this actions')
 
     # Room has been actually deleted
-    if( request.method == 'POST' ):
+    if(request.method == 'POST'):
         room.delete()
         return redirect('home')
-        
+
     return render(request, 'base/delete.html', context)
